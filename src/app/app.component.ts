e2119e021962222445c1b791f4bd3450ee0b09ff
Ryan Strandt <ryan.strandt@cru.org>
2017-08-24 14:42:12 -0400

    setTimeout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  processing = 'processing...';
+  disabled = false;
+  input = [1,2,3,4,5,6,7,8,9,10];
+
+  runProcess(){
+    let n = 0;
+    let timer = [];
+    while(n != this.input.length){
+      setTimeout((i)=>{
+        // console.log(input[n]);
+        this.processing = this.processing.concat(i.toString(),', ');
+        console.log(this.processing)
+        console.log(i.toString());
+      },500, this.input[n])
+      n++;
+    }
+    this.disabled = !this.disabled;
+  }
+
 }
